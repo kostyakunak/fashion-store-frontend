24427ad2b770eb0053c7e13a32dfea174fb72683
kostyakunak <koc9ihbi4@gmail.com>
2025-02-23 11:41:18 +0100

    Third Version. the project is adjusted for React. Needs to be improved: - product payment page - fix CSS in item.css - fix CSS differences in headers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Account from "./components/Account";
+import OrderDetails from "./components/OrderDetails";
+import Home from "./components/Home";
+import Catalog from "./components/Catalog";
+import Contacts from "./components/Contacts";
+import MyOrders from "./components/MyOrders";
+import Wishlist from "./components/Wishlist";
+import Item from "./components/Item";
+import Cart from "./components/Cart";
+import AccountDetails from "./components/AccountDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Router>
+        <div className="App">
+          {/* Навигация теперь будет доступна на всех страницах */}
 
-      </header>
-    </div>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/order-details" element={<OrderDetails />} />
+            <Route path="/catalog" element={<Catalog />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/my-orders" element={<MyOrders />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/item" element={<Item />} />
+              <Route path="/cart" element={<Cart />} />
+            <Route path="/account-details" element={<AccountDetails />} />
+          </Routes>
+
+
+        </div>
+      </Router>
+
   );
 }
 
